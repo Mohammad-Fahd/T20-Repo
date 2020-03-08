313f424733194586bcf67d117e1897e3341eceec
Yuan223 <60626156+Yuan223@users.noreply.github.com>
2020-03-07 23:42:48 -0700

    Failed obstacle

--- Obstacle.java
+++ Obstacle.java
@@ -1,31 +1,80 @@
+  
+package project;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
-public class Obstacle {
+public class Obstacle extends Sprite {
 	private String name;
 	//Data type of speed is set in int. You can only move in the board composed by arry which indexes are all integer numbers
 	// The data type of speed will fixed before moving in the diagram 
-	private int speed; 
+	private double speed; 
 	private int xCood, yCood;
 	
-	//Regular Constructor to create a new obstacle iterm
-	public Obstacle (int x, int y, int Speed){
-		this.xCood = x;
-		this.yCood = y;
+	//Regular Constructor to create a new obstacle item
+	public Obstacle (int x, int y, double Speed){
+		super(x,y,Color.RED);
 		this.speed = Speed;
 				
 	}
-	public void move() {
+	
+	public Obstacle (int x, int y) {
+		super(x,y);
+		this.speed = getSpeed();
+	}
+	
+	public Obstacle (Obstacle o) {
+		super(o);
+		this.speed = o.getSpeed();
+	}
+	//Setters an Getters
+	public int getX() {
+		return(xCood);
+	}
+	
+	public int getY() {
+		return(yCood);
+	}
+	
+	public double getSpeed()
+	{
+		return speed;
+	}
+	
+	public void updateSpeed(int Speed)
+	{
+		speed = Speed;
+	}
+	
+	//Obstacle moving
+		public Obstacle moveR() {
+			int newX = this.xCood;
+			int newY = this.yCood;
+			double newS = this.speed;
+			newX += newS;
+			if (newX >= 10) {
+				newX -= 1;
+			}
+			return(new Obstacle(newX, newY));
+		}
+		
+	//Obstacle moving
+	public Obstacle moveL() {
 		int newX = this.xCood;
 		int newY = this.yCood;
 		int newS = this.speed;
-		newX += newS;
-		if (newX <= -1 || newX >= 15) {
-			newX -= 1;
+		newX -= newS;
+		if (newX <= -1 ) {
+			newX += 1;
 		}
-		
+		return(new Obstacle(newX, newY));
 	}
+	
 	//overlapsWith method to check if there is a intersect between Avatar and Obstacle
 	public boolean overlapsWith(Avatar a) {
-		if (a.xCoord == xCood && a.yCoord == yCood)
+		if (a.getXCoord() == xCood && a.getYCoord() == yCood)
 		{
 			return true;
 		}
@@ -34,9 +83,17 @@ public class Obstacle {
 			return false;
 		}
 	}
-	public boolean isDeadly() {
-		
-	}
+	//public boolean isDeadly(Avatar a) {
+		//if (a.getyCood()>=1 && a.getyCood()<=8 && overlapsWith(a) == true) {
+			//return true;
+		//}
+		//else if (a.getyCood()>=9 && a.getyCood()<=14 && overlapsWith(a) == false) {
+			//return true;
+		//}
+		//else {
+			//return false;
+		//}
+	//}
 	//The data type of multipletimes is int, the seem reason as the data type of speed should be int 
 	public void accelerate(int multipletimes) {
 		int newX = this.xCood;
