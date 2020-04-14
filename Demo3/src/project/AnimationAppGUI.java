937ee61506883d735d6313e099189492c23bf73f
Mohammad-Fahd <60626056+Mohammad-Fahd@users.noreply.github.com>
2020-04-14 09:09:29 -0600

    Update AnimationAppGUI.java

--- Demo3/src/project/AnimationAppGUI.java
+++ Demo3/src/project/AnimationAppGUI.java
@@ -9,9 +9,13 @@ import javafx.scene.paint.*;
 import javafx.stage.Stage;
 import javafx.util.Duration;
  
+//Controller that combines logic classes w/ GUI classes
 public class AnimationAppGUI extends Application {
+ //Creates arrayLists to store multiple Boxes (logic class) and Sprites (GUI class) 
     ArrayList<Box> objects = new ArrayList<Box>();
     ArrayList<Sprite> visual = new ArrayList<Sprite>();
+ 
+ //variables that stores size of window
     final int boardX = 550;
     final int boardY = 750;
    
@@ -21,33 +25,61 @@ public class AnimationAppGUI extends Application {
         try {
             //Creation of window
             Pane root = new Pane();
+         
+         //Creates main menu object that shows at start and when player goes back
             MainScreen mS = new MainScreen();
+         
+         //Creates game over object that shows when player's health reaches 0
             GOPopup GameOver = new GOPopup();
+         
+         //Sets size of window
             root.setPrefSize(boardX, boardY);
+         
+         //Sets background colour of window
             root.setStyle("-fx-background-color: lightgreen");
+         
+         //Creates avatar (logic) class and attaches sprite to it to translate movements onto window
             Avatar avatar = new Avatar(350,700);
             Sprite player = new Sprite(avatar);
+         
+         //Creates GameUI object that shows player score and health on top left
             GameUI userInterface = new GameUI(avatar);
+         
+         //Creates obsctacles and end goals as shown in methods below
             createObstacles();
             createEnds();
+         
+         //Creates collectible in the middle of "river" level and attaches sprite to it
             Collectible collectible = new Collectible(250,200,300,1);
             Sprite pickup = new Sprite(collectible);
+         
+         //Adds river collectible to arrayLists (collectible class in objects, sprite class in visual)
             objects.add(collectible);
             visual.add(pickup);
+         
+         //Adds all sprites in visual arrayList to screen
             for (Sprite i: visual)
             {
                 root.getChildren().add(i);
             }
+         
+         //Adds player to screen (drawn last so as to be displayed on top of all other objects)
             root.getChildren().add(player);
             userInterface.establishLabels(root);
+         
+         //Creates scenes for main game, main menu, and game over screen
             Scene scene = new Scene(root,550,750);
             Scene mScene = new Scene(mS,550,750);
             Scene GOScreen = new Scene(GameOver, 550, 750);
+         
+         //Checks for keyboard input and moves player character accordingly
             avatar.setMovement(scene);
+         
+         //Adds avatar to arrayLists
             objects.add(avatar);
             visual.add(player);
            
-            //Timeline that moves non-player characters and detects collisions
+            //Timeline that moves non-player characters and detects collisions, as well as button presses on main menu and game over screen
             Timeline timeline1 = new Timeline(
                     new KeyFrame(Duration.millis(17),
                            new EventHandler <ActionEvent>()
@@ -55,14 +87,24 @@ public class AnimationAppGUI extends Application {
                             @Override
                             public void handle(ActionEvent event)
                             {
+                             
+                             //Checks for button presses on main menu screen
                                 mS.Interaction(primaryStage, scene);
+                             
+                             //Looks through each box object and checks to see if it has collided with avatar
+                             //Then performs appropriate action as defined in their classes
                                 for (Box i : objects) {
                                     i.Interaction(avatar);
                                 }
+                             
+                             //Resets collectibles when player ends up back at the start
                                 if(avatar.getX() == 350 && avatar.getY() == 700)
                                 {
                                     collectible.resetCollectible(objects);
                                 }
+                             
+                             //Changes x and y of GUI classes according to the positions determined in logic classes
+                             //Also applies any colour changes to collectibles
                                 for(int i = 0;i < visual.size(); i++)
                                 {
                                     visual.get(i).setX(objects.get(i).getX());
@@ -73,6 +115,8 @@ public class AnimationAppGUI extends Application {
                                     }
                                 }
                                 userInterface.updateLabels();
+                             
+                             //Checks to see if player has lost all their lives, then goes to game over screen
                                 if(avatar.getHealth() == 0) {
                                     primaryStage.setScene(GOScreen);
                                     GameOver.Interaction(primaryStage, mScene, scene, avatar);
@@ -82,11 +126,13 @@ public class AnimationAppGUI extends Application {
                     )
                 );
            
+         //Plays timeline
             timeline1.setCycleCount(Timeline.INDEFINITE);
             timeline1.setAutoReverse(true);
             timeline1.play();
             //x = 550px, y = 750px
-            //scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+         
+         //Sets game to be displayed on main screen
             primaryStage.setScene(mScene);
             primaryStage.setTitle("Frogger");
             primaryStage.show();
