bdadd75c56125ec42566c6d994a884824dc8fabc
Mohammad-Fahd <60626056+Mohammad-Fahd@users.noreply.github.com>
2020-04-14 08:51:02 -0600

    Update imageButton.java

--- Demo3/src/project/imageButton.java
+++ Demo3/src/project/imageButton.java
@@ -8,15 +8,19 @@ import javafx.scene.input.MouseEvent;
 public class imageButton extends Button {
  
     public void updateImages(final Image clicked, final Image notClicked) {
+     //Adds image to button for when it is not clicked
         ImageView cButton = new ImageView(notClicked);
         this.getChildren().add(cButton);
+     //Sets background to be transparent
         this.setStyle("-fx-background-color: transparent;");
+     //When pressed, changes button to a different image indicating that it has been clicked
         this.setOnMousePressed(new EventHandler<MouseEvent>() {
             public void handle(MouseEvent event) {
                 cButton.setImage(clicked);
             }
         });
        
+     //When button is released, goes back to "unclicked" image
         this.setOnMouseReleased(new EventHandler<MouseEvent>() {
             public void handle(MouseEvent event) {
                 cButton.setImage(notClicked);
