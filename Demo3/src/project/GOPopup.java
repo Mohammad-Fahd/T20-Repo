1fc7ca2432a15ad7146a7b928a92b32e6cab2e79
Mohammad-Fahd <60626056+Mohammad-Fahd@users.noreply.github.com>
2020-04-13 21:26:43 -0600

    Update GOPopup.java

--- Demo3/src/project/GOPopup.java
+++ Demo3/src/project/GOPopup.java
@@ -24,11 +24,11 @@ public class GOPopup extends Pane {
    
     public GOPopup() {
         try {
-            GOTitle = new Image(new FileInputStream("C:\\Users\\Ec_71\\Desktop\\GOText.png"));
-            contBNC = new Image(new FileInputStream("C:\\Users\\Ec_71\\Desktop\\contBnotClicked.png"));
-            contBC = new Image(new FileInputStream("C:\\Users\\Ec_71\\Desktop\\contBClicked.png"));
-            mSBNC = new Image(new FileInputStream("C:\\Users\\Ec_71\\Desktop\\mSBnotClicked.png"));
-            mSBC = new Image(new FileInputStream("C:\\Users\\Ec_71\\Desktop\\mSBClicked.png"));
+            GOTitle = new Image(new FileInputStream("images\GOText.png"));
+            contBNC = new Image(new FileInputStream("images\contBnotClicked.png"));
+            contBC = new Image(new FileInputStream("images\contBClicked.png"));
+            mSBNC = new Image(new FileInputStream("images\mSBnotClicked.png"));
+            mSBC = new Image(new FileInputStream("images\mSBClicked.png"));
         } catch (Exception e) {
             System.out.println("ERROR: PATH NOT FOUND");
         }
