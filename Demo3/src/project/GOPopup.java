7eaf40c2c65962f77f63132e45f592d02f5678bb
Mohammad-Fahd <60626056+Mohammad-Fahd@users.noreply.github.com>
2020-04-14 08:40:56 -0600

    Update GOPopup.java

--- Demo3/src/project/GOPopup.java
+++ Demo3/src/project/GOPopup.java
@@ -9,40 +9,53 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
  
+//Class that handles Game Error and checking to see if the Avatar has run out of lives
 public class GOPopup extends Pane {
+ //Width and height of Game Over Popup
     final int boardX = 550;
     final int boardY = 750;
    
+ //Images that are used to create the game over screen
     private Image contBNC;
     private Image contBC;
     private Image mSBNC;
     private Image mSBC;
     private Image GOTitle;
+ 
+ //Buttons to handle restarting or going back to main menu
     imageButton Continue = new imageButton();
     imageButton BacktoMS = new imageButton();
     private ImageView cImage = new ImageView();
    
+ //Constructor
     public GOPopup() {
         try {
+         //Sets image variables to correct file path
             GOTitle = new Image(new FileInputStream("images/GOText.png"));
             contBNC = new Image(new FileInputStream("images/contBnotClicked.png"));
             contBC = new Image(new FileInputStream("images/contBClicked.png"));
             mSBNC = new Image(new FileInputStream("images/mSBnotClicked.png"));
             mSBC = new Image(new FileInputStream("images/mSBClicked.png"));
+         //Throws error if file path is not found (file path is relative so must be in correct location compared to AnimationAppGUI)
         } catch (Exception e) {
             System.out.println("ERROR: PATH NOT FOUND");
         }
+     //Sets colour of background
         setStyle("-fx-background-color: darkgreen");
+     //Sets size of window
         setPrefSize(boardX, boardY);
+     //Places game over screen in a vertical box object
         VBox winLayout = new VBox();
+     //Sets images for going back to main menu or restarting
         Continue.updateImages(contBC, contBNC);
         BacktoMS.updateImages(mSBC, mSBNC);
-       
+     //Sets width and height of title  
         cImage.setImage(GOTitle);
         cImage.setFitHeight(100);
         cImage.setFitWidth(550);
         cImage.setLayoutY(20);
        
+     //Adds buttons to vertical box object
         winLayout.getChildren().add(Continue);
         winLayout.getChildren().add(BacktoMS);
         winLayout.layoutXProperty().bind(widthProperty().subtract(winLayout.widthProperty()).divide(2));
@@ -53,11 +66,14 @@ public class GOPopup extends Pane {
        
     }
    
+ //Activates when player is dead (has 0 lives) to show game over screen. Then handles button presses
     public void Interaction(Stage cStage, Scene mainScreen, Scene Game, Avatar a) {
         Continue.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
+             //Go back to game
                 cStage.setScene(Game);
+             //Reset player stats
                 a.updateHealth(3);
                 a.updateScore(a.getScore()*-1);
             }
@@ -65,7 +81,9 @@ public class GOPopup extends Pane {
         BacktoMS.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
+             //Go back to main screen
                 cStage.setScene(mainScreen);
+             //Reset player stats
                 a.updateHealth(3);
                 a.updateScore(a.getScore()*-1);
             }
